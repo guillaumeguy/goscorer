e7c794e3af904f8fce76ce23da1a4171ea2ae6ec
Guillaume Guy <gguy@lyft.com>
2020-04-06 11:03:01 -0700

    remoe solution for R model

--- generate_pmml.R
+++ generate_pmml.R
@@ -7,12 +7,5 @@ library('randomForest')
 
 names(iris) = c('sepal_length_cm', 'sepal_width_cm', 'petal_length_cm', 'petal_width_cm','species')
 
-iris$is_setosa <- as.factor(ifelse(iris$species == 'setosa',1,0))
-
-iris_rf <- randomForest(is_setosa ~ sepal_length_cm + sepal_width_cm + petal_length_cm + petal_width_cm,
-                           data = iris,
-                          ntree = 20
-                           )
-pmml_obj <- pmml(iris_rf)
 
 saveXML(pmml_obj, "~/go/src/goscorer/static/iris.pmml")
